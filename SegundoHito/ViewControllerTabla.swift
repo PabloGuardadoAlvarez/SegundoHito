4d39115d8c7b0ae133adadb3fd5c3cb0a46a1d71
PabloGuardadoAlvarez <pablo.guardado@live.u-tad.com>
2018-04-17 12:13:02 +0200

    Tabla creada correctamente (sin celda prototipo)

---
+++ SegundoHito/ViewControllerTabla.swift
@@ -0,0 +1,47 @@
+//
+//  ViewControllerTabla.swift
+//  SegundoHito
+//
+//  Created by PABLO GUARDADO ALVAREZ on 17/4/18.
+//  Copyright © 2018 PABLO GUARDADO ALVAREZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTabla: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    
+    @IBOutlet var tabla:UITableView?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "primeracelda")!
+        return cell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
